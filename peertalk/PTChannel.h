5e616e3736856b5c6524278ae260fc6a23ef2e0a
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-04-22 17:23:31 +0200

    Add nullability annotations to the channel artifact

--- peertalk/PTChannel.h
+++ peertalk/PTChannel.h
@@ -10,6 +10,8 @@
 #import "PTProtocol.h"
 #import "PTUSBHub.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @class PTData, PTAddress;
 @protocol PTChannelDelegate;
 
@@ -29,7 +31,7 @@
 
 // Arbitrary attachment. Note that if you set this, the object will grow by
 // 8 bytes (64 bits).
-@property (strong) id userInfo;
+@property (strong, nullable) id userInfo;
 
 // Create a new channel using the shared PTProtocol for the current dispatch
 // queue, with *delegate*.
@@ -49,26 +51,26 @@
 
 
 // Connect to a TCP port on a device connected over USB
-- (void)connectToPort:(int)port overUSBHub:(PTUSBHub*)usbHub deviceID:(NSNumber*)deviceID callback:(void(^)(NSError *error))callback;
+- (void)connectToPort:(int)port overUSBHub:(PTUSBHub*)usbHub deviceID:(NSNumber*)deviceID callback:(nullable void(^)(NSError *error))callback;
 
 // Connect to a TCP port at IPv4 address. Provided port must NOT be in network
 // byte order. Provided in_addr_t must NOT be in network byte order. A value returned
 // from inet_aton() will be in network byte order. You can use a value of inet_aton()
 // as the address parameter here, but you must flip the byte order before passing the
 // in_addr_t to this function.
-- (void)connectToPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^)(NSError *error, PTAddress *address))callback;
+- (void)connectToPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(nullable void(^)(NSError *error, PTAddress *address))callback;
 
 // Listen for connections on port and address, effectively starting a socket
 // server. Provided port must NOT be in network byte order. Provided in_addr_t
 // must NOT be in network byte order.
 // For this to make sense, you should provide a onAccept block handler
 // or a delegate implementing ioFrameChannel:didAcceptConnection:.
-- (void)listenOnPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^)(NSError *error))callback;
+- (void)listenOnPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(nullable void(^)(NSError *error))callback;
 
 // Send a frame with an optional payload and optional callback.
 // If *callback* is not NULL, the block is invoked when either an error occured
 // or when the frame (and payload, if any) has been completely sent.
-- (void)sendFrameOfType:(uint32_t)frameType tag:(uint32_t)tag withPayload:(dispatch_data_t)payload callback:(void(^)(NSError *error))callback;
+- (void)sendFrameOfType:(uint32_t)frameType tag:(uint32_t)tag withPayload:(nullable dispatch_data_t)payload callback:(nullable void(^)(NSError *error))callback;
 
 // Lower-level method to assign a connected dispatch IO channel to this channel
 - (BOOL)startReadingFromConnectedChannel:(dispatch_io_t)channel error:(__autoreleasing NSError**)error;
@@ -108,7 +110,7 @@
 
 @required
 // Invoked when a new frame has arrived on a channel.
-- (void)ioFrameChannel:(PTChannel*)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(PTData*)payload;
+- (void)ioFrameChannel:(PTChannel*)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(nullable PTData*)payload;
 
 @optional
 // Invoked to accept an incoming frame on a channel. Reply NO ignore the
@@ -117,10 +119,12 @@
 
 // Invoked when the channel closed. If it closed because of an error, *error* is
 // a non-nil NSError object.
-- (void)ioFrameChannel:(PTChannel*)channel didEndWithError:(NSError*)error;
+- (void)ioFrameChannel:(PTChannel*)channel didEndWithError:(nullable NSError*)error;
 
 // For listening channels, this method is invoked when a new connection has been
 // accepted.
 - (void)ioFrameChannel:(PTChannel*)channel didAcceptConnection:(PTChannel*)otherChannel fromAddress:(PTAddress*)address;
 
 @end
+
+NS_ASSUME_NONNULL_END
